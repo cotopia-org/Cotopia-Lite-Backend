8fb8c0bad0f901ff52c0cc2fdc4bd890c64e932d
Taban Kharrati <tabankharrati@gmail.com>
2024-05-21 13:28:35 +0330

    CHANGE user model

--- db/models/user.py
+++ db/models/user.py
@@ -1,22 +1,19 @@
-import enum as pyEnum
-
-from sqlalchemy import Boolean, Column, Enum, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String
 
 from ..db_setup import Base
 from .mixins import Timestamp
-
-
-class Role(pyEnum.IntEnum):
-    default = 1
 
 
 class User(Timestamp, Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(100), unique=True, index=True, nullable=False)
+    username = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String(511))
-    role = Column(Enum(Role), nullable=False, default=1)
     is_active = Column(Boolean, default=True)
     first_name = Column(String(50), nullable=True)
     last_name = Column(String(50), nullable=True)
+    email = Column(String(63), nullable=True)
+    status = Column(String(31), nullable=True)
+    avatar = Column(String(255), nullable=True)
+    bio = Column(String(255), nullable=True)
