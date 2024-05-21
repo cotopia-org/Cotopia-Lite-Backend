ef806199819682bc89d3fb319f1fdb6be26e248e
Taban Kharrati <tabankharrati@gmail.com>
2024-05-21 13:40:17 +0330

    CHANGE class name

--- db/models/room.py
+++ db/models/room.py
@@ -4,7 +4,7 @@ from ..db_setup import Base
 from .mixins import Timestamp
 
 
-class User(Timestamp, Base):
+class Room(Timestamp, Base):
     __tablename__ = "rooms"
 
     id = Column(Integer, primary_key=True, index=True)
