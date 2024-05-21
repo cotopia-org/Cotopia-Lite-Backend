b99c9d29e756e61c3aa6df33fc6a8406d08af302
Taban Kharrati <tabankharrati@gmail.com>
2024-05-21 21:00:30 +0330

    ADD workspace model

---
+++ db/models/workspace.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+
+from ..db_setup import Base
+from .mixins import Timestamp
+from sqlalchemy.orm import relationship
+
+
+class Workspace(Timestamp, Base):
+    __tablename__ = "workspaces"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    user = relationship("User")
+    name = Column(String(100), unique=True, index=True, nullable=False)
+    description = Column(String(255), nullable=True)
+    is_active = Column(Boolean, default=True)
+    is_private = Column(Boolean, default=False)
+    avatar = Column(String(255), nullable=True)
+    banner = Column(String(255), nullable=True)
