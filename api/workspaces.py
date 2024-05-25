ae07c9b6cf7a5f67b5c99ffbc0491bf44ca3f95c
Taban Kharrati <tabankharrati@gmail.com>
2024-05-25 14:59:14 +0330

    ADD API update_workspace

--- api/workspaces.py
+++ api/workspaces.py
@@ -37,8 +37,19 @@ async def get_workspace_by_id(workspace_id: int,
 
 
 @router.put("/workspace/{workspace_id}", response_model=Workspace, status_code=200)
-async def update_workspace():
-    pass
+async def update_workspace(workspace_id: int,
+                           workspace: WorkspaceUpdate,
+                            current_user: Annotated[User, Depends(get_current_active_user)],
+                            db: Session = Depends(get_db),
+                            ):
+    db_workspace = get_ws_by_id(db=db, workspace_id=workspace_id)
+    if db_workspace is None:
+        raise HTTPException(status_code=404, detail=f"Workspace (id = {workspace_id}) not found!")
+    else:
+        if db_workspace.user_id == current_user.id:
+            return edit_ws(db=db, workspace_id=workspace_id, workspace=workspace)
+        else:
+            raise HTTPException(status_code=403, detail="You are not the creator of this workspace!")
 
 @router.delete("/workspace/{workspace_id}", response_model=Workspace, status_code=204)
 async def delete_workspace():
