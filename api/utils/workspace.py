c6dedf1dc580773370c80862733478b780b20631
Taban Kharrati <tabankharrati@gmail.com>
2024-05-25 14:05:32 +0330

    ADD edit workspace util

--- api/utils/workspace.py
+++ api/utils/workspace.py
@@ -1,6 +1,7 @@
 from db.models.workspace import Workspace as WorkspaceModel
 from sqlalchemy.orm import Session
-from schemas.workspace import WorkspaceCreate
+from schemas.workspace import WorkspaceCreate, WorkspaceUpdate
+import datetime
 
 
 def create_ws(db: Session, workspace: WorkspaceCreate, user_id: int):
@@ -17,12 +18,22 @@ def create_ws(db: Session, workspace: WorkspaceCreate, user_id: int):
 def get_ws_by_id(db: Session, workspace_id: int):
     return db.query(WorkspaceModel).filter(WorkspaceModel.id == workspace_id).first()
 
+
 def get_ws_by_user(db: Session, user_id: int):
     return db.query(WorkspaceModel).filter(WorkspaceModel.user_id == user_id).all()
 
 
-def edit_ws():
-    pass
+def edit_ws(db: Session, workspace_id: int, workspace: WorkspaceUpdate):
+    db_workspace = db.query(WorkspaceModel).get(workspace_id)
+    db_workspace.updated_at = datetime.datetime.now(datetime.timezone.utc)
+
+    for var, value in vars(workspace).items():
+        if value:
+            setattr(db_workspace, var, value)
+
+    db.add(db_workspace)
+    db.commit()
+    return db_workspace
 
 
 def delete_ws():
