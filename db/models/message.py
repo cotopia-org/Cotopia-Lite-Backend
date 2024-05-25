1ebc619894e2ac3f129d2b3c18a812c4335e81a0
Taban Kharrati <tabankharrati@gmail.com>
2024-05-25 13:23:09 +0330

    FIX TEXT size limit

--- db/models/message.py
+++ db/models/message.py
@@ -2,17 +2,14 @@ from sqlalchemy import Boolean, Column, Integer, ForeignKey, Text
 
 from ..db_setup import Base
 from .mixins import Timestamp
-from sqlalchemy.orm import relationship
 
 
 class Message(Timestamp, Base):
     __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
-    user = relationship("User")
-    room_id = Column(Integer, ForeignKey("rooms.id"), primary_key=True)
-    room = relationship("Room")
+    user_id = Column(Integer, ForeignKey("users.id"))
+    room_id = Column(Integer, ForeignKey("rooms.id"))
     reply_to = Column(Integer, nullable=True)
     edited = Column(Boolean, default=False)
-    text = Column(Text(2047))
+    text = Column(Text)
