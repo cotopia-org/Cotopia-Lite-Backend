313e551714b36a03f022263a3265136dc42f8530
Taban Kharrati <tabankharrati@gmail.com>
2024-05-28 12:07:44 +0330

    WIP room utils

---
+++ api/utils/room.py
@@ -0,0 +1,21 @@
+from db.models.room import Room as RoomModel
+from sqlalchemy.orm import Session
+import datetime
+from schemas.room import RoomCreate, RoomUpdate
+
+
+
+def create_da_room():
+    pass
+
+def get_da_room_by_id():
+    pass
+
+def get_da_rooms_by_workspace():
+    pass
+
+def edit_da_room():
+    pass
+
+def delete_da_room():
+    pass
