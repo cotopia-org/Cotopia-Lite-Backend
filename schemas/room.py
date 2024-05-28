91ee790c8f7594cab3278b44d5545616a80f557f
Taban Kharrati <tabankharrati@gmail.com>
2024-05-28 12:02:50 +0330

    ADD room shcemas

---
+++ schemas/room.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class RoomBase(BaseModel):
+    workspace_id: int
+    
+
+class RoomCreate(RoomBase):
+    title: str
+
+class RoomUpdate(RoomCreate):
+    title: str | None = None
+    status: str | None = None
+    is_locked: bool | None = None
+    passcode :str | None = None
+    avatar: str | None = None
+    background_image: str | None = None
+
+class Room(RoomUpdate):
+    id: int
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
