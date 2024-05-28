64cb7d8bb3e7933c0ef4b3a4e5e5a0c74205d7f2
Taban Kharrati <tabankharrati@gmail.com>
2024-05-28 19:52:34 +0330

    WIP room_user utils

---
+++ api/utils/room_user.py
@@ -0,0 +1,21 @@
+from db.models.room_user import RoomUser as RoomUserModel
+from sqlalchemy.orm import Session
+import datetime
+from schemas.room_user import RoomUserCreate, RoomUserUpdate
+
+
+def ru_join():
+    pass
+
+
+def get_ru():
+    pass
+
+
+def edit_ru():
+    pass
+
+
+def ru_leave():
+    pass
+
