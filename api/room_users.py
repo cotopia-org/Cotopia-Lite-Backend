4d3ef2b5c45d942bc9eba73e4ae6e0af670236f8
Taban Kharrati <tabankharrati@gmail.com>
2024-05-29 16:28:41 +0330

    ADD leave_room endpoint

--- api/room_users.py
+++ api/room_users.py
@@ -24,6 +24,24 @@ async def join_room(
 ):
     return create_ru(db=db, room_user=room_user, room_id=room_id, user_id=current_user.id)
 
-# Leaving the room
+
+@router.delete("/leave_room", status_code=204)
+async def delete_room(
+    room_id: int,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    db: Session = Depends(get_db),
+):
+    db_room_user = get_ru(db=db, room_id=room_id, user_id=current_user.id)
+    if db_room_user is None:
+        raise HTTPException(status_code=404, detail=f"RoomUser (room_id = {room_id}, user_id = {current_user.id}) not found!")
+    else:
+        if True:  # check permission
+            delete_ru(db=db, room_id=room_id, user_id=current_user.id)
+        else:
+            raise HTTPException(
+                status_code=403,
+                detail="You do not have permission to perform this action!",
+            )
+
 
 #  Room users status
