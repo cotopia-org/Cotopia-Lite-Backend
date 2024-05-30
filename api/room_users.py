86eb77510c45c86f8fca30fe13a3987754391dc7
Taban Kharrati <tabankharrati@gmail.com>
2024-05-30 15:26:34 +0330

    FORMAT file

--- api/room_users.py
+++ api/room_users.py
@@ -1,17 +1,14 @@
-import time
 from typing import Annotated
 
 import fastapi
-from fastapi import Depends, HTTPException, WebSocket, WebSocketDisconnect
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from api.utils.room_user import create_ru, get_ru, edit_ru, delete_ru
+from api.utils.room_user import create_ru, delete_ru, edit_ru, get_ru
 from auth import get_current_active_user
 from db.db_setup import get_db
-
+from schemas.room_user import RoomUser, RoomUserCreate, RoomUserUpdate
 from schemas.user import User
-from schemas.room_user import RoomUserCreate, RoomUserUpdate, RoomUser
-
 
 router = fastapi.APIRouter()
 
@@ -23,7 +20,9 @@ async def join_room(
     current_user: Annotated[User, Depends(get_current_active_user)],
     db: Session = Depends(get_db),
 ):
-    return create_ru(db=db, room_user=room_user, room_id=room_id, user_id=current_user.id)
+    return create_ru(
+        db=db, room_user=room_user, room_id=room_id, user_id=current_user.id
+    )
 
 
 @router.delete("/room/{room_id}/leave", status_code=204)
@@ -34,7 +33,10 @@ async def delete_room(
 ):
     db_room_user = get_ru(db=db, room_id=room_id, user_id=current_user.id)
     if db_room_user is None:
-        raise HTTPException(status_code=404, detail=f"RoomUser (room_id = {room_id}, user_id = {current_user.id}) not found!")
+        raise HTTPException(
+            status_code=404,
+            detail=f"RoomUser (room_id = {room_id}, user_id = {current_user.id}) not found!",
+        )
     else:
         if True:  # check permission
             delete_ru(db=db, room_id=room_id, user_id=current_user.id)
@@ -56,10 +58,15 @@ async def update_ru(
     # Change status of others based on role
     db_room_user = get_ru(db=db, room_id=room_id, user_id=current_user.id)
     if db_room_user is None:
-        raise HTTPException(status_code=404, detail=f"RoomUser (room_id = {room_id}, user_id = {current_user.id}) not found!")
+        raise HTTPException(
+            status_code=404,
+            detail=f"RoomUser (room_id = {room_id}, user_id = {current_user.id}) not found!",
+        )
     else:
         if True:  # check permission
-            return edit_ru(db=db, room_id=room_id, user_id=current_user.id, room_user=room_user)
+            return edit_ru(
+                db=db, room_id=room_id, user_id=current_user.id, room_user=room_user
+            )
         else:
             raise HTTPException(
                 status_code=403,
@@ -67,13 +74,12 @@ async def update_ru(
             )
 
 
-
 #  Room users status
 
 # class ConnectionManager:
 #     def __init__(self):
 #         self.active_connections: list[WebSocket] = []
-    
+
 #     async def connect(self, websocket: WebSocket):
 #         await websocket.accept()
 #         self.active_connections.append(websocket)
@@ -83,19 +89,19 @@ async def update_ru(
 
 #     async def send_personal_message(self, message: str, websocket: WebSocket):
 #         await websocket.send_text(message)
-    
+
 #     async def broadcast(self, message: str):
 #         for connection in self.active_connections:
 #             await connection.send_text(message)
 
-    
+
 # manager = ConnectionManager()
 
 
 # @router.websocket("/room_status/{room_id}")
 # async def room_status(room_id: int, websocket: WebSocket):
 #     await manager.connect(websocket)
-#     try: 
+#     try:
 #         while True:
 #             data = await websocket.receive_json()
 #             await manager.broadcast(data)
