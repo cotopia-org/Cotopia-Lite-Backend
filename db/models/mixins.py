b85180688bcfa6460bc9276f297cb14d8f952c49
katerou22 <xkaterou22@gmail.com>
2024-05-30 11:10:03 +0330

    Fix utc get now in mixins

--- db/models/mixins.py
+++ db/models/mixins.py
@@ -6,5 +6,5 @@ from sqlalchemy.orm import declarative_mixin
 
 @declarative_mixin
 class Timestamp:
-    created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC), nullable=False)
-    updated_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.datetime.now(), nullable=False)
