c42e73a39a64426640f91f0333ee7c49e35931f5
Ali Kharrati <ali.kharrati@gmail.com>
2024-05-30 12:46:09 +0330

    FIX module 'datetime' has no attribute 'UTC'

--- db/models/mixins.py
+++ db/models/mixins.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import timezone
 
 from sqlalchemy import Column, DateTime
 from sqlalchemy.orm import declarative_mixin
@@ -6,5 +7,5 @@ from sqlalchemy.orm import declarative_mixin
 
 @declarative_mixin
 class Timestamp:
-    created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC), nullable=False)
-    updated_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now(timezone.utc), nullable=False)
+    updated_at = Column(DateTime, default=datetime.datetime.now(timezone.utc), nullable=False)
