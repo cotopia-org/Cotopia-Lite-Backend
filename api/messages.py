0a19625a6236d61153269dc671ebc7a069ad3cfe
Taban Kharrati <tabankharrati@gmail.com>
2024-06-04 15:37:00 +0330

    ADD /messages/{message_id}/update endpoint

--- api/messages.py
+++ api/messages.py
@@ -5,10 +5,10 @@ from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from api.utils.auth import get_current_active_user
-from api.utils.message import create_msg, get_room_msgs, edit_msg, delete_msg
+from api.utils.message import create_msg, get_room_msgs, edit_msg, delete_msg, get_msg_by_id
 from db.db_setup import get_db
 from db.models import Message as MessageModel
-from schemas.message import Message, MessageCreate
+from schemas.message import Message, MessageCreate, MessageUpdate
 from schemas.user import User
 
 router = fastapi.APIRouter()
@@ -34,3 +34,24 @@ async def get_messages(
 ):
     messages = get_room_msgs(db=db, room_id=room_id, skip=skip, limit=limit)
     return messages
+
+
+@router.put("/messages/{message_id}/update", response_model=Message, status_code=200)
+async def edit_message(
+    message_id: int,
+    message: MessageUpdate,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    db: Session = Depends(get_db),
+):
+    db_message = get_msg_by_id(db=db, message_id=message_id)
+    if db_message is None:
+        raise HTTPException(
+            status_code=404, detail=f"Message (id = {message_id}) not found!"
+        )
+    else:
+        if db_message.user_id == current_user.id:
+            return edit_msg(db=db, message_id=message_id, message=message)
+        else:
+            raise HTTPException(
+                status_code=403, detail="You are not the author of this message!"
+            )
