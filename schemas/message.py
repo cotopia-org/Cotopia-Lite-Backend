7b52fc87770187d0ffccbb4d0dd31760117ef75f
Taban Kharrati <tabankharrati@gmail.com>
2024-06-04 13:42:16 +0330

    ADD MessageCreate, MessageUpdate schemas

--- schemas/message.py
+++ schemas/message.py
@@ -7,21 +7,24 @@ from schemas.user import User
 
 
 class MessageBase(BaseModel):
-    id: int
+    user_id: int
+    room_id: int
+
+
+class MessageCreate(MessageBase):
+    text: str
+    reply_to: int | None = None
+
+
+class MessageUpdate(MessageBase):
+    text: str
 
 
 class Message(MessageBase):
-    user_id: int
+    id: int
     user: User
-
-    room_id: int
     room: Room
-
-    reply_to: int | None
-
     edited: bool
-    text: str
-
     created_at: datetime
     updated_at: datetime | None = None
 
