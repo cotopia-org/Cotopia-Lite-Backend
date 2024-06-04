89ebdb1d941c4c214cf9db9ce66c0b1f0e3ce6ca
Taban Kharrati <tabankharrati@gmail.com>
2024-06-04 13:18:55 +0330

    FIX allow updated_at to be None

--- schemas/room_user.py
+++ schemas/room_user.py
@@ -30,7 +30,7 @@ class RoomUserUpdate(RoomUserCreate):
 class RoomUser(RoomUserUpdate):
     is_active: bool
     created_at: datetime
-    updated_at: datetime
+    updated_at: datetime | None = None
 
     class Config:
         orm_mode = True
