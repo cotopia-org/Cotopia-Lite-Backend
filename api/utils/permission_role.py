91aa6407388fbd96bb32ec30f7a15d3ed3f0c9b2
Taban Kharrati <tabankharrati@gmail.com>
2024-06-05 19:13:16 +0330

    ADD delete permission_role util

--- api/utils/permission_role.py
+++ api/utils/permission_role.py
@@ -1,5 +1,3 @@
-import datetime
-
 from sqlalchemy.orm import Session
 
 from db.models import PermissionRole as PRModel
@@ -22,13 +20,15 @@ def get_da_pr_by_id(db: Session, permission_role_id: int):
 
 
 def get_da_pr(db: Session, permission_id: int, role_id: int):
-    return db.query(PRModel).filter(PRModel.permission_id == permission_id, PRModel.role_id == role_id).first()
+    return (
+        db.query(PRModel)
+        .filter(PRModel.permission_id == permission_id, PRModel.role_id == role_id)
+        .first()
+    )
 
 
-
-
-def delete_da_role(db: Session, role_id: int):
-    db_role = db.query(RoleModel).get(role_id)
-    db.delete(db_role)
+def delete_da_pr(db: Session, permission_role_id: int):
+    db_permission_role = db.query(PRModel).get(permission_role_id)
+    db.delete(db_permission_role)
     db.commit()
-    return db_role
+    return db_permission_role
