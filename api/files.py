bdb2a817ed88a2d420cf5bf9514e5f301afedf62
Taban Kharrati <tabankharrati@gmail.com>
2024-06-12 09:07:07 +0330

    CHANGE move helper to file

--- api/files.py
+++ api/files.py
@@ -1,56 +1,5 @@
 import fastapi
-from pathlib import Path
-
-from fastapi import HTTPException, status
-from typing import IO
-import filetype
-
-UPLOAD_DIR = Path() / "uploads"
-MAX_FILE_SIZE = 2 * 2097152  # 4MB
-ALLOWED_FILE_TYPES = [
-    "image/png",
-    "image/jpeg",
-    "image/jpg",
-    "image/heic",
-    "image/heif",
-    "image/heics",
-    "png",
-    "jpeg",
-    "jpg",
-    "heic",
-    "heif",
-    "heics",
-]
-
-
-def validate_file(file: IO):
-
-    file_info = filetype.guess(file.file)
-    if file_info is None:
-        raise HTTPException(
-            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            detail="Unable to determine file type",
-        )
-
-    detected_content_type = file_info.extension.lower()
-
-    if (
-        file.content_type not in ALLOWED_FILE_TYPES
-        or detected_content_type not in ALLOWED_FILE_TYPES
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            detail="Unsupported file type!",
-        )
-
-    real_file_size = 0
-    for chunk in file.file:
-        real_file_size += len(chunk)
-        if real_file_size > MAX_FILE_SIZE:
-            raise HTTPException(
-                status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, detail="File too large!"
-            )
-
+from api.utils.file import validate_file, UPLOAD_DIR
 
 router = fastapi.APIRouter()
 
